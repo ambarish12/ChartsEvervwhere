5ea1f678ac921643f678ff6b3de63559e3ea2b5d
Aishwarya Devi <aishwaryadevi@Aishwaryas-MacBook-Air.local>
2022-08-25 17:05:20 +0530

    magnitude bar chart working

--- charts-ui/src/App.js
+++ charts-ui/src/App.js
@@ -1,15 +1,18 @@
 import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
-import { drawChart, initChart } from './charts/BasicD3';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 import BarChart from './charts/BarChart';
 
 function App() {
-  const [ eq_data, setEqData ] = useState([]);
+  const [eq_data, setEqData] = useState([]);
   const [data, setData] = useState([]);
-  let fieldToMap = "eqMagnitude";
+  const flds = ["eqMagnitude", "deaths", "eqDepth"];
+  const [fieldToMap, setField] = useState(flds[0]);
 
   useEffect(() => {
-    // initChart(400, 600);
     const getData = async () => {
       const dataFromServer = await fetchData();
       setEqData(dataFromServer);
@@ -18,20 +21,25 @@ function App() {
     getData();
   }, []);
 
-  useEffect(()=> {
+  useEffect(() => {
     changeData();
-  }, [eq_data]);
-  
+  }, [eq_data, fieldToMap]);
+
   const changeData = () => {
-    // console.log("CHANGEDATA: ", i);
-    setData(eq_data.map(d => d[fieldToMap] || 0));
-    // if(i === dataset.length) i = 0;
+    const data = eq_data.sort((a, b) => {
+      return b[fieldToMap] - a[fieldToMap];
+    }).filter((d, index) => index < 10);
+    setData(data.map(d => {
+      return {
+        x: d.year,
+        y: d[fieldToMap]
+      }
+    }));
   }
 
-  const changeField = (e) => {
-    fieldToMap = e.target.value;
-    console.log('field', fieldToMap)
-  }
+  const handleChange = (event) => {
+    setField(event.target.value);
+  };
 
   const fetchData = async () => {
     const res = await fetch('http://localhost:5000/eq');
@@ -43,14 +51,22 @@ function App() {
   return (
     <div className="App">
       {eq_data.map((d, index) => <div key={index}>{d.location_name}</div>)}
-      <Button variant="contained">Sample button</Button>
 
       <div className="charts-div">
         <h2>Charts</h2>
-        <input onChange={changeField}/>
-        <Button variant="outlined" onClick={changeData}>Change Data</Button>
-        {/* <div id="chart"></div> */}
-        <BarChart width={600} height={400} data={data} />
+        <FormControl>
+          <InputLabel id="demo-simple-select-label">Field</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={fieldToMap}
+            label="Field"
+            onChange={handleChange}
+          >
+            {flds.map((d, index) => <MenuItem key={index} value={d}>{d}</MenuItem>)}
+          </Select>
+        </FormControl>
+        {data.length > 0 && <BarChart width1={600} height1={400} data={data} />}
       </div>
     </div>
   );
--- charts-ui/src/charts/BarChart.js
+++ charts-ui/src/charts/BarChart.js
@@ -1,62 +1,74 @@
-import * as d3 from 'd3';
 import React, { useRef, useEffect } from 'react';
+import * as d3 from 'd3';
 
-function BarChart({ width, height, data }){
+function BarChart(props) {
     const ref = useRef();
-
     useEffect(() => {
-        const svg = d3.select(ref.current)
-            .attr("width", width)
-            .attr("height", height)
-            .style("border", "1px solid black")
-    }, []);
+        var svg = d3.select(ref.current),
+            margin = 200,
+            width = svg.attr("width") - margin,
+            height = svg.attr("height") - margin
 
-    useEffect(() => {
-        draw();
-    }, [data]);
 
-    const draw = () => {
-        
-        const svg = d3.select(ref.current);
-        var selection = svg.selectAll("rect").data(data);
-        var yScale = d3.scaleLinear()
-                            .domain([0, d3.max(data)])
-                            .range([0, height-100]);
-        
-        selection
-            .transition().duration(300)
-                .attr("height", (d) => yScale(d))
-                .attr("y", (d) => height - yScale(d))
+        svg.append("text")
+            .attr("transform", "translate(100,0)")
+            .attr("x", 50)
+            .attr("y", 50)
+            .attr("font-size", "24px")
+            .text("Top 10 Earthquakes by Magnitude")
 
-        selection
-            .enter()
-            .append("rect")
-            .attr("x", (d, i) => i * 45)
-            .attr("y", (d) => height)
-            .attr("width", 40)
-            .attr("height", 0)
-            .attr("fill", "orange")
-            .transition().duration(300)
-                .attr("height", (d) => yScale(d))
-                .attr("y", (d) => height - yScale(d))
-        
-        selection
+        var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+            yScale = d3.scaleLinear().range([height, 0]);
+
+        var g = svg.append("g")
+            .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+
+        xScale.domain(props.data.map(function (d) { return d.x; }));
+        // yScale.domain([0, d3.max(props.data, function (d) { return d.y; })]);
+        yScale.domain([0, 10]);
+
+        g.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(xScale))
+            .append("text")
+            .attr("y", height - 250)
+            .attr("x", width - 100)
+            .attr("text-anchor", "end")
+            .attr("stroke", "black")
+            .text("Year");
+
+        g.append("g")
+            .call(d3.axisLeft(yScale).tickFormat(function (d) {
+                return d;
+            })
+                .ticks(10))
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("x", -100)
+            .attr("y", 6)
+            .attr("dy", "-5.1em")
+            .attr("text-anchor", "end")
+            .attr("stroke", "black")
+            .text("Stock Price");
+
+        let group = g.selectAll(".bar")
+            .data(props.data);
+        group
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) { return xScale(d.x); })
+            .attr("y", function (d) { return yScale(d.y); })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function (d) { return height - yScale(d.y); });
+
+        group
             .exit()
-            .transition().duration(300)
-                .attr("y", (d) => height)
-                .attr("height", 0)
             .remove()
-    }
-
-
+    }, [props.data]);
     return (
-        <div className="chart">
-            <svg ref={ref}>
-            </svg>
-        </div>
-        
+        <svg ref={ref} width="600" height="500"></svg>
     )
-
 }
 
-export default BarChart;
+export default BarChart
